a74a2cbe6ca4d3fe60c637177dfa9df46e0a41e5
dserero <daserboss@gmail.com>
2025-11-26 13:12:10 +0100

    fix vulnerabilities

---
+++ eslint.config.js
@@ -0,0 +1,4 @@
+const nextCoreWebVitals = require("eslint-config-next/core-web-vitals");
+const nextTypeScript = require("eslint-config-next/typescript");
+
+module.exports = [...nextCoreWebVitals, ...nextTypeScript];
